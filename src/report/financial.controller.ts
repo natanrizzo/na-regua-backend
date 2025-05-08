af0a6756a534b43ace8f9e956eabe59dbf515611
natanrizzo <natan.rizzo22@gmail.com>
2025-05-08 20:43:30 -0300

    ♻️  refactor(financial) implemented roles guard instead of policies guard

--- src/report/financial.controller.ts
+++ src/report/financial.controller.ts
@@ -2,38 +2,32 @@ import { Controller, Get, UseGuards } from "@nestjs/common";
 import { FinancialService } from "./financial.service";
 import { CurrentUser } from "src/auth/currentUser.decorator";
 import { User } from "generated/prisma";
-import { CheckPolicies } from "src/casl/policies/policies.decorator";
-import { PoliciesGuard } from "src/casl/policies/policies.guard";
-import { ReadFinancialPolicy } from "./policy/readFinancial.policy";
+import { RolesGuard } from "src/auth/roles/roles.guard";
+import { Roles } from "src/auth/roles/roles.decorator";
+import { Role } from "src/auth/roles/role.enum";
 
 @Controller('reports/financial')
-@UseGuards(PoliciesGuard)
+@UseGuards(RolesGuard)
 export class FinancialController {
     constructor(
         private readonly financialService: FinancialService
     ) {}
 
     @Get('daily')
-    @CheckPolicies(new ReadFinancialPolicy())
-    async getRevenueByDay(
-        @CurrentUser() user: User
-    ) {
-        return await this.financialService.getRevenueByDay(user);
+    @Roles(Role.Administrator)
+    async getRevenueByDay() {
+        return await this.financialService.getRevenueByDay();
     }
 
     @Get('by-appointment')
-    @CheckPolicies(new ReadFinancialPolicy())
-    async getRevenueByAppointment(
-        @CurrentUser() user: User
-    ) {
-        return await this.financialService.getRevenueByAppointment(user);
+    @Roles(Role.Administrator)
+    async getRevenueByAppointment() {
+        return await this.financialService.getRevenueByAppointment();
     }
 
     @Get('by-product')
-    @CheckPolicies(new ReadFinancialPolicy())
-    async getRevenueByProduct(
-        @CurrentUser() user: User
-    ) {
-        return await this.financialService.getRevenueByProduct(user);
+    @Roles(Role.Administrator)
+    async getRevenueByProduct() {
+        return await this.financialService.getRevenueByProduct();
     }
 }
