89b75b6931a046045565e145d0c4b2eb3f99f4b4
natanrizzo <natan.rizzo22@gmail.com>
2025-05-08 20:52:12 -0300

    💱 feat(transaction) implemented transaction controller create, get all, get one, update routes

--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -1,9 +1,43 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Get, Param, Patch, Post } from "@nestjs/common";
 import { TransactionService } from "./transaction.service";
+import { CreateTransactionDTO } from "./dto/createTransaction.dto";
+import { CurrentUser } from "src/auth/currentUser.decorator";
+import { User } from "generated/prisma";
+import { UpdateTransactionDTO } from "./dto/updateTransaction.dto";
 
-@Controller()
+@Controller('transaction')
 export class TransactionController {
     constructor(
         private readonly transactionService: TransactionService
     ) {}
+
+    @Post('/')
+    async createTransaction(
+        @Body() createTransactionDTO: CreateTransactionDTO
+    ) {
+        return this.transactionService.createTransaction(createTransactionDTO);
+    }
+
+    @Get('/')
+    async getTransactions(
+        @CurrentUser() user: User,
+    ) {
+        return this.transactionService.getTransactions(user);
+    }
+
+    @Get('/:id')
+    async getTransactionById(
+        @Param('id') id: string,
+    ) {
+        return this.transactionService.getTransactionById(id);
+    }
+
+    @Patch('/:id')
+    async updateTransaction(
+        @Body() updateTransactionDTO: UpdateTransactionDTO,
+        @Param('id') id: string,
+
+    ) {
+        return this.transactionService.updateTransaction(id, updateTransactionDTO);
+    }
 }
